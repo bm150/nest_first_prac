e8fc4b00ab28921c31b1bf4496e452b322d1a6f7
bm150 <jbm20dev@gmail.com>
2025-07-27 21:33:44 +0900

    Feat : board 게시글 등록 기능 추가

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,10 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { Board, BoardStatus } from './board.model';
+import { v1 as uuid } from 'uuid';
 
 @Injectable()
 export class BoardsService {
-    private boards = ['1'];
+    private boards: Board[] = [];
     
-    getAllBoards(){
+    getAllBoards() : Board[]{
         return this.boards;
+    }
+
+    createBoard(title : string ,description:string){
+        const board : Board = {
+            title,
+            description,
+            status : BoardStatus.PUBLIC,
+            id : uuid()
+        }
+        this.boards.push(board);
+        return board;
     }
 }
